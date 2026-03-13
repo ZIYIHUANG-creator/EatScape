a79b4056d0b8036a2e4f8e7715fbb89b321d5d03
ZIYIHUANG-creator <hzy200636@qq.com>
2026-03-13 18:02:33 +0800

    将部署文件放到根目录，终极修复空白

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// 配置 GitHub Pages 专用路径
-export default {
-  base: '/EatScape/',
+export default defineConfig({
   plugins: [vue()],
-}
+  base: './'
+})
